e9e89cd6ff0a376faf62e2868e702fcd0f54a541
KobzarMax <nimbus2219@gmail.com>
2023-05-23 08:14:08 +0200

    FEAT: timer

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -30,12 +30,22 @@ export const Timer = () => {
 
   function getRemainingTime(endDate) {
     const total = Date.parse(endDate) - Date.now()
-    const seconds = Math.floor((total / 1000) % 60)
+    const seconds = 60
     const minutes = Math.floor((total / 1000 / 60) % 60)
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
     const days = Math.floor(total / (1000 * 60 * 60 * 24))
     return { total, days, hours, minutes, seconds }
   }
+
+  function calculateTrailLength(unit, totalDuration) {
+    const remainingTimeValue = remainingTime[unit]
+    const trailLength =
+      ((totalDuration - remainingTimeValue) / totalDuration) * 100
+    return trailLength.toFixed(2)
+  }
+
+  const totalDays = 24 * 60 * 60 // Assuming 24 hours per day
+  const totalHours = 60 * 60 // Assuming 60 minutes per hour
 
   return (
     <div className="timer relative z-10 mr-[30px] mt-[269px] lg:absolute lg:right-[47px] lg:top-[255px] lg:mr-0 lg:mt-0">
@@ -45,9 +55,12 @@ export const Timer = () => {
       <div className="flex items-center justify-center gap-2.5">
         <CountdownCircleTimer
           isPlaying
-          duration={remainingTime.days}
+          duration={remainingTime.days * 60 * 60 * 24}
           colors={[['#FF1B5E']]}
-          trailColor={'#1B1B1B80'}
+          trailColor={`conic-gradient(#1B1B1B80 ${calculateTrailLength(
+            'days',
+            totalDays
+          )}%, transparent 0%)`}
           strokeWidth={6}
           size={80}
           onComplete={() => {
@@ -69,9 +82,12 @@ export const Timer = () => {
         </CountdownCircleTimer>
         <CountdownCircleTimer
           isPlaying
-          duration={remainingTime.hours}
+          duration={remainingTime.hours * 60 * 60}
           colors={[['#FF1B5E']]}
-          trailColor={'#1B1B1B80'}
+          trailColor={`conic-gradient(#1B1B1B80 ${calculateTrailLength(
+            'hours',
+            totalHours
+          )}%, transparent 0%)`}
           strokeWidth={6}
           size={80}
           onComplete={() => {
@@ -93,9 +109,12 @@ export const Timer = () => {
         </CountdownCircleTimer>
         <CountdownCircleTimer
           isPlaying
-          duration={remainingTime.minutes}
+          duration={remainingTime.minutes * 60}
           colors={[['#FF1B5E']]}
-          trailColor={'#1B1B1B80'}
+          trailColor={`conic-gradient(#1B1B1B80 ${calculateTrailLength(
+            'minutes',
+            60
+          )}%, transparent 0%)`}
           strokeWidth={6}
           size={80}
           onComplete={() => {
@@ -119,7 +138,7 @@ export const Timer = () => {
           isPlaying
           duration={remainingTime.seconds}
           colors={[['#FF1B5E']]}
-          trailColor={'#1B1B1B80'}
+          trailColor={`#1B1B1B80 ${calculateTrailLength('seconds', 60)}%`}
           strokeWidth={6}
           size={80}
           onComplete={() => {
