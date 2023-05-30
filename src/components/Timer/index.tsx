13590a2042961c0720803b335574610eddd1c683
Andrii <A.I.Khmelovsky@gmail.com>
2023-05-30 14:15:26 +0300

    Refactored countdown

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,47 +1,33 @@
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
-import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+
+import { useCountdown } from 'hooks/useCountdown'
+
+const minuteSeconds = 60
+const hourSeconds = 3600
+const daySeconds = 86400
 
 export const Timer = () => {
   const { t } = useTranslation(['home'])
-  const startDate = new Date('07/01/2023')
-  const [countdownDate] = useState(startDate)
-  const [remainingTime, setRemainingTime] = useState(
-    getRemainingTime(countdownDate)
-  )
+  const [days, hours, minutes, seconds] = useCountdown('07/01/2023')
+  const daysDuration = days * daySeconds
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      const newRemainingTime = getRemainingTime(countdownDate)
-      setRemainingTime(newRemainingTime)
+  const stratTime = Date.now()
+  const targetDate = new Date('07/01/2023')
 
-      if (newRemainingTime.total <= 0) {
-        clearInterval(timer)
-      }
-    }, 1000)
+  const diffTime = Math.abs(targetDate - stratTime) / 1000
 
-    return () => clearInterval(timer)
-  }, [countdownDate])
+  const endTime = stratTime + diffTime
 
-  function getRemainingTime(endDate) {
-    const total = Date.parse(endDate) - Date.now()
-    const seconds = 60
-    const minutes = Math.floor((total / 1000 / 60) % 60)
-    const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
-    const days = Math.floor(total / (1000 * 60 * 60 * 24))
-    return { total, days, hours, minutes, seconds }
-  }
+  const remainingTime = endTime - stratTime
 
-  function calculateTrailLength(unit, totalDuration) {
-    const remainingTimeValue = remainingTime[unit]
-    const trailLength =
-      ((totalDuration - remainingTimeValue) / totalDuration) * 100
-    return trailLength.toFixed(2)
-  }
+  const getTimeSeconds = (time: number) => (minuteSeconds - time) | 0
+  const getTimeMinutes = (time: number) =>
+    ((time % hourSeconds) / minuteSeconds) | 0
+  const getTimeHours = (time: number) => ((time % daySeconds) / hourSeconds) | 0
+  const getTimeDays = (time: number) => (time / daySeconds) | 0
 
-  const totalDays = 24 * 60 * 60 // Assuming 24 hours per day
-  const totalHours = 60 * 60 // Assuming 60 minutes per hour
-
+  console.log('TIMER', days, hours, minutes, seconds)
   return (
     <div className="timer relative z-10 mr-[30px] mt-[269px] lg:absolute lg:right-[47px] lg:top-[255px] lg:mr-0 lg:mt-0">
       <p className="mb-2.5 text-center text-[15px] leading-[18px] text-white">
@@ -50,23 +36,17 @@ export const Timer = () => {
       <div className="flex items-center justify-center gap-2.5">
         <CountdownCircleTimer
           isPlaying
-          duration={remainingTime.days * 60 * 60 * 24}
+          duration={daysDuration}
+          initialRemainingTime={remainingTime}
           colors={[['#FF1B5E']]}
-          trailColor={`conic-gradient(#1B1B1B80 ${calculateTrailLength(
-            'days',
-            totalDays
-          )}%, transparent 0%)`}
           strokeWidth={6}
           size={80}
-          onComplete={() => {
-            return { shouldRepeat: true, delay: 1 } // repeat animation in 1 second
-          }}
         >
-          {({ remainingTime }) => (
+          {({ elapsedTime }) => (
             <div className="timer-wrapper">
               <div className="timer-text flex flex-col items-center justify-center">
                 <span className="text-primary text-[25px] font-semibold leading-[30px]">
-                  {(remainingTime / 60 / 60 / 24).toString().split('.')[0]}
+                  {getTimeDays(daysDuration - elapsedTime)}
                 </span>
                 <span className="text-[13px] leading-4 text-white">
                   {t('days')}
@@ -75,25 +55,20 @@ export const Timer = () => {
             </div>
           )}
         </CountdownCircleTimer>
+
         <CountdownCircleTimer
           isPlaying
-          duration={remainingTime.hours * 60 * 60}
+          duration={daySeconds}
           colors={[['#FF1B5E']]}
-          trailColor={`conic-gradient(#1B1B1B80 ${calculateTrailLength(
-            'hours',
-            totalHours
-          )}%, transparent 0%)`}
+          initialRemainingTime={remainingTime % daySeconds}
           strokeWidth={6}
           size={80}
-          onComplete={() => {
-            return { shouldRepeat: true, delay: 1 } // repeat animation in 1 second
-          }}
         >
-          {({ remainingTime }) => (
+          {({ elapsedTime }) => (
             <div className="timer-wrapper">
               <div className="timer-text flex flex-col items-center justify-center">
                 <span className="text-primary text-[25px] font-semibold leading-[30px]">
-                  {(remainingTime / 60 / 60).toString().split('.')[0]}
+                  {getTimeHours(daySeconds - elapsedTime)}
                 </span>
                 <span className="text-[13px] leading-4 text-white">
                   {t('hours')}
@@ -102,25 +77,20 @@ export const Timer = () => {
             </div>
           )}
         </CountdownCircleTimer>
+
         <CountdownCircleTimer
           isPlaying
-          duration={remainingTime.minutes * 60}
+          duration={hourSeconds}
           colors={[['#FF1B5E']]}
-          trailColor={`conic-gradient(#1B1B1B80 ${calculateTrailLength(
-            'minutes',
-            60
-          )}%, transparent 0%)`}
+          initialRemainingTime={remainingTime % hourSeconds}
           strokeWidth={6}
           size={80}
-          onComplete={() => {
-            return { shouldRepeat: true, delay: 1 } // repeat animation in 1 second
-          }}
         >
-          {({ remainingTime }) => (
+          {({ elapsedTime }) => (
             <div className="timer-wrapper">
               <div className="timer-text flex flex-col items-center justify-center">
                 <span className="text-primary text-[25px] font-semibold leading-[30px]">
-                  {(remainingTime / 60).toString().split('.')[0]}
+                  {getTimeMinutes(hourSeconds - elapsedTime)}
                 </span>
                 <span className="text-[13px] leading-4 text-white">
                   {t('minutes')}
@@ -131,20 +101,21 @@ export const Timer = () => {
         </CountdownCircleTimer>
         <CountdownCircleTimer
           isPlaying
-          duration={remainingTime.seconds}
+          duration={minuteSeconds}
+          initialRemainingTime={remainingTime % minuteSeconds}
           colors={[['#FF1B5E']]}
-          trailColor={`#1B1B1B80 ${calculateTrailLength('seconds', 60)}%`}
+          trailColor={`conic-gradient(#1B1B1B80 ${null}%, transparent 0%)`}
           strokeWidth={6}
           size={80}
           onComplete={() => {
             return { shouldRepeat: true, delay: 1 } // repeat animation in 1 second
           }}
         >
-          {({ remainingTime }) => (
+          {({ elapsedTime }) => (
             <div className="timer-wrapper">
               <div className="timer-text flex flex-col items-center justify-center">
                 <span className="text-primary text-[25px] font-semibold leading-[30px]">
-                  {remainingTime.toString().split('.')[0]}
+                  {getTimeSeconds(elapsedTime)}
                 </span>
                 <span className="text-[13px] leading-4 text-white">
                   {t('seconds')}
