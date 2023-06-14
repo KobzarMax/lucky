f6f02b84f0d6f6df9fe7bb88c34cfce4a6987bbe
Maks Kobzar <maksym.kobzar@vao.pl>
2023-06-14 14:32:11 +0200

    FEAT: rules page scroll part change

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -4,40 +4,42 @@ function Rules() {
   const { t } = useTranslation(['translations'])
   return (
     <div className="min-h-[100vh] pt-[90px]">
-      <div className="rules-text-wrapper mx-auto h-[565px] max-w-[765px] overflow-y-auto pl-[13px] pr-3 lg:px-0">
+      <div className="mx-auto max-w-[765px] pl-[13px] pr-3 lg:px-0">
         <h1 className="mb-[15px] text-center text-[25px] font-semibold uppercase leading-[30px] text-white">
           {t('rulesTitle')}
         </h1>
-        <p className="mb-[30px] text-[15px] leading-[18px] text-white">
-          {t('rulesSubtitle')}
-        </p>
-        <p className="mb-2.5 text-[15px] font-medium leading-[18px] text-[#1B6AF5]">
-          {t('basicRules')}
-        </p>
-        <p className="mb-4 text-[15px] leading-[18px] text-white">
-          {t('basicRules11')}
-        </p>
-        <p className="mb-4 text-[15px] leading-[18px] text-white">
-          {t('basicRules12')}
-        </p>
-        <p className="mb-4 text-[15px] leading-[18px] text-white">
-          {t('basicRules13')}
-        </p>
-        <p className="mb-2.5 text-[15px] font-medium leading-[18px] text-[#1B6AF5]">
-          {t('rightsTitle')}
-        </p>
-        <p className="mb-4 text-[15px] leading-[18px] text-white">
-          {t('rights21')}
-        </p>
-        <p className="mb-4 text-[15px] leading-[18px] text-white">
-          {t('rights22')}
-        </p>
-        <p className="mb-4 text-[15px] leading-[18px] text-white">
-          {t('rights23')}
-        </p>
-        <p className="mb-2.5 text-[15px] font-medium leading-[18px] text-[#1B6AF5]">
-          {t('confTitle')}
-        </p>
+        <div className="rules-text-wrapper h-[50vh] overflow-y-auto lg:h-[580px]">
+          <p className="mb-[30px] text-[15px] leading-[18px] text-white">
+            {t('rulesSubtitle')}
+          </p>
+          <p className="mb-2.5 text-[15px] font-medium leading-[18px] text-[#1B6AF5]">
+            {t('basicRules')}
+          </p>
+          <p className="mb-4 text-[15px] leading-[18px] text-white">
+            {t('basicRules11')}
+          </p>
+          <p className="mb-4 text-[15px] leading-[18px] text-white">
+            {t('basicRules12')}
+          </p>
+          <p className="mb-4 text-[15px] leading-[18px] text-white">
+            {t('basicRules13')}
+          </p>
+          <p className="mb-2.5 text-[15px] font-medium leading-[18px] text-[#1B6AF5]">
+            {t('rightsTitle')}
+          </p>
+          <p className="mb-4 text-[15px] leading-[18px] text-white">
+            {t('rights21')}
+          </p>
+          <p className="mb-4 text-[15px] leading-[18px] text-white">
+            {t('rights22')}
+          </p>
+          <p className="mb-4 text-[15px] leading-[18px] text-white">
+            {t('rights23')}
+          </p>
+          <p className="mb-2.5 text-[15px] font-medium leading-[18px] text-[#1B6AF5]">
+            {t('confTitle')}
+          </p>
+        </div>
       </div>
     </div>
   )
