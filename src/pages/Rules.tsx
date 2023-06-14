bd85f1b8b973457a0ea9eabe0dd1cbde140ce89c
Andrii <A.I.Khmelovsky@gmail.com>
2023-06-14 16:56:27 +0300

    height FIX

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -1,14 +1,44 @@
+import { FC, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import MobileDetect from 'mobile-detect';
 
-function Rules() {
+const Rules: FC = () => {
   const { t } = useTranslation(['translations'])
+  const [height, setHeight] = useState<number>(0)
+
+  useEffect(() => {
+    const calculateHeight = () => {
+      const windowHeight = window.innerHeight
+      const md = new MobileDetect(window.navigator.userAgent);
+      const isMobile = md.mobile();
+      let calculatedHeight = windowHeight - 250;
+
+      if (isMobile) {
+        calculatedHeight -= 100; 
+      }
+      setHeight(calculatedHeight)
+    }
+
+    calculateHeight()
+
+    window.addEventListener('resize', calculateHeight)
+
+    return () => {
+      window.removeEventListener('resize', calculateHeight)
+    }
+  }, [])
+
   return (
     <div className="min-h-[100vh] pt-[90px]">
       <div className="mx-auto max-w-[765px] pl-[13px] pr-3 lg:px-0">
         <h1 className="mb-[15px] text-center text-[25px] font-semibold uppercase leading-[30px] text-white">
           {t('rulesTitle')}
         </h1>
-        <div className="rules-text-wrapper h-[55vh] overflow-y-auto">
+
+        <div
+          style={{ height: `${height}px` }}
+          className={`rules-text-wrapper overflow-y-auto`}
+        >
           <p className="mb-[30px] text-[15px] leading-[18px] text-white">
             {t('rulesSubtitle')}
           </p>
